9e2af4c3b2b525616dbef83087d3c2f8772da884
Erich Purpur <epurpur@gmail.com>
2020-07-06 16:07:21 -0400

    section 3 updates

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,18 +1,35 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+/*
+calculateAge(1965);   //this is stored before code is executed. Don't have to first declare function before using
+
+function calculateAge(year) {
+    console.log(2016 - year);
+}
+
+calculateAge(1999);
 
 
+//retirement(1956);    // this function is not yet hoisted
 
+var retirement  = function(year) {
+    console.log(65 - (2016-year));
+}
 
+retirement(1990);
 
+console.log(age);
+var age = 23;
 
+function foo() {
+    var age = 65;
+    console.log(age);
+}
 
-
-
-
-
-
+foo();
+console.log(age);
+*/
 
 
 
@@ -68,9 +85,44 @@ function third() {
 // Lecture: The this keyword
 
 
+//console.log(this);  //points to window object (global)
+
+/*
+function calculateAge(year) {
+    console.log(2016 - year);
+    console.log(this);     //points to window object (global)
+}
+
+calculateAge(1985);
+*/
+
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2020 - this.yearOfBirth);
+
+        /*
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+        */
+    }
+}
+
+john.calculateAge();
 
 
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+}
 
+mike.calculateAge = john.calculateAge;   //treat the function like a variable
+
+mike.calculateAge();
 
 
 
