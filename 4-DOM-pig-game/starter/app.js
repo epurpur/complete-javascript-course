9ce990f45a74074cfa816277032c0d2058f8b7e7
Erich Purpur <epurpur@gmail.com>
2020-07-27 08:37:41 -0400

    bday coding

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,51 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];  /* both player scores set in array */
+round = 0;
+activePlayer = 0; 
+
+
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+	
+	//1. random number
+	var dice = Math.floor(Math.random() * 6) + 1;
+
+	//2. Display result
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+
+
+
+	//3. Update round score if rolled number was NOT a 1
+
+
+})
+
+
+
+/* Puts dice value into DOM for activePlayer*/
+//document.querySelector('#current-' + activePlayer).textContent = dice;   //.textContent sets text only
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';  //em for italics.  .innerHTML sets HTML and text
+
+
+/* math object, javascript built in object */
+//dice = Math.floor(Math.random() * 6) + 1;
+//console.log(dice);
+
+
+
+
