4adb77e93a4b886a0aa94dc09387e2af0dc97d59
Erich Purpur <epurpur@gmail.com>
2020-09-08 12:01:47 -0400

    ch 9

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,1 +1,8 @@
 // Global app controller
+
+import num from './test';
+
+const x = 23;
+
+console.log(`I imported ${num} from another module called test.js Varaible x is ${x}`);
+console.log('hello world IS THIS WORKING?')
---
+++ 9-forkify/starter/src/js/test.js
@@ -0,0 +1,4 @@
+
+console.log('Imported Module');
+
+export default 123456;
